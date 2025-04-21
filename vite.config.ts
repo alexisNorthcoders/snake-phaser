0e7f6cb0bfec05cb4c54bcedb9e24c7d56a7b888
alexis.creoulo <alexiscreoulo@hotmail.com>
2025-04-21 10:42:28 +0000

    script update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-export default {
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  }
-  
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
